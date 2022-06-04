ddcd3cda396876e7581ae8c21c4d95c3c73c3412
Maurice Schwarz <schwarz@desginpark.de>
2022-06-04 19:35:05 +0200

    Update layout.tsx

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -5,7 +5,9 @@ export default function Layout({ children }:any) {
   return (
     <>
       {/* <Navbar /> */}
-      <main>{children}</main>
+      <main className='class="text-center text-gray-700 dark:text-gray-200 dark:bg-gray-900"'>
+        {children}
+      </main>
       <Footer />
     </>
   )
