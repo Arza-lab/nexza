57df6c396fca2bc91b483fa77eb82bafe1bc0248
Maurice Schwarz <schwarz@desginpark.de>
2022-06-04 15:50:37 +0200

    feat: added @mui

--- windi.config.js
+++ windi.config.js
@@ -5,4 +5,15 @@ export default defineConfig({
     include: ["**/*.{jsx,tsx,css}"],
     exclude: ["node_modules", ".git", ".next"],
   },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+        serif: ["Roboto Serif", "sans-serif"],
+        mono: ["Roboto Mono", "sans-serif"],
+        display: ["Oswald"],
+        body: ["Open Sans"],
+      },
+    },
+  },
 });
